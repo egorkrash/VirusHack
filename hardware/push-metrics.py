3e9ea487f0de74794954ab7c01e4b76073bb02fa
Sergey Zakharov <mrsergeycity@gmail.com>
2020-05-04 19:49:04 +0300

    update snmp emulator

--- hardware/push-metrics.py
+++ hardware/push-metrics.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import random
+
+import numpy as np
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+
 hostname = "localhost:9091"
 # num from 1-10
 num = random.randint(1, 10)
@@ -11,5 +14,18 @@ g = Gauge(
     registry=registry)
 # g.set_to_current_time()
 g.set(num)
+# push_to_gateway(hostname, job='batchB', registry=registry)
+print("num:", num)
+
+temperature = 31
+cpu_load = np.random.lognormal(mean=3, sigma=.4)
+mem_load = np.random.lognormal(mean=3, sigma=.4)
+metrics = [(temperature, "temperature", "Device temperature"),
+           (cpu_load, "cpu_load", "CPU load"),
+           (mem_load, "mem_load", "Memory load")]
+for metric in metrics:
+    # registry = CollectorRegistry()
+    g = Gauge(metric[1], metric[2], registry=registry)
+    g.set(metric[0])
+    print(metric[1], metric[0])
 push_to_gateway(hostname, job='batchB', registry=registry)
-print(num)
