3430f9c87b924cd48d2df3cf24c70779f3e11805
Nikolay Nazarov <nazarov_ni@mail.ru>
2020-05-04 23:26:30 +0300

    add timeControler

--- frontend/alarms/src/containers/Alerts/index.js
+++ frontend/alarms/src/containers/Alerts/index.js
@@ -1,8 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import React, {useEffect, useState} from 'react';
-import { Col, Container, Row, Alert, Button } from "reactstrap";
+import { Col, Container, Row, Button } from "reactstrap";
 import defaults from "../../config";
 import Header from "../../components/Header";
+import TimeControler from "../../components/TimeControler"
 
 
 function get_alarm_bar({data, title}) {
@@ -24,21 +25,27 @@ function get_alarm_bar({data, title}) {
   }
   return (
     <Col style={{width: '90%', backgroundColor: '#EFEFEF', margin: 3}}>
-      <p><b> {title} </b></p>
+      <p><b> {title} ({users.length}) </b></p>
       {res}
     </Col>
   );
 }
 
 
-async function fetch_data({set_data, hour}) {
+async function fetch_data({set_data, hour, set_fetchDone}) {
+  const hour_text = defaults.hour_textHour[hour.hour.toString()].slice(5, 13)
+  // alert(hour_text);
   const requestOptions = {
     method: 'POST',
-    body: JSON.stringify({hour: hour.hour})
+    body: JSON.stringify({hour: hour_text})
   };
+  set_fetchDone(false);
   fetch('http://localhost:4500/get_alert_users', requestOptions)
     .then(response => response.json())
-    .then(data => set_data(data));
+    .then(data => {
+      set_fetchDone(true);
+      set_data(data)
+    });
 }
 
 
@@ -49,11 +56,12 @@ const Alerts = () => {
     tr_inp_zero_alert: [[], []],
     tr_out_zero_alert: [[], []]
   });
-  const [hour, set_hour] = useState({hour: '05-03 00'})
+  const [hour, set_hour] = useState({hour: 24});
+  const [fetchDone, set_fetchDone] = useState(true)
 
   useEffect(() => {
-    fetch_data({set_data, hour}).then();
-  },   [1]);
+    fetch_data({set_data, hour, fetchDone, set_fetchDone}).then();
+  },   [hour]);
 
 
   // alert(JSON.stringify(data)); f
@@ -69,9 +77,14 @@ const Alerts = () => {
     <div>
       <Header/>
       <Container>
+        <Row >
+          <Col sm={{ size: 4, order: 1, offset: 8 }}>
+            <TimeControler hour={hour} set_hour={set_hour} fetchDone={fetchDone} />
+          </Col>
+        </Row>
         <Row> {columns_alerts} </Row>
       </Container>
-    </div>
+     </div>
   );
 };
 
